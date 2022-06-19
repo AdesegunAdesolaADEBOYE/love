20b3c63c5ff6ae790bb1cbac2d22867384689d39
Adesegun Adesola ADEBOYE <fantasticinkmedia@gmail.com>
2022-06-19 06:10:42 +0100

    Add files via upload

---
+++ WhatsApp.py
@@ -0,0 +1,5 @@
+import pywhatkit
+from pywhatkit.remotekit import start_server
+from flask import Flask, request
+pywhatkit.sendwhatmsg('+2347089107759', 'Thank you for all the sacrifices', 16, 24)
+
